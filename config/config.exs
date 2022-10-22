fa31dd375f06293105a1da41b4ffe7d4ee1e2cce
Finn Behrens <me@kloenk.dev>
2022-10-23 00:11:00 +0200

    update nixos module for database

--- config/config.exs
+++ config/config.exs
@@ -10,6 +10,8 @@ config :jlly_bot,
   namespace: JllyBot,
   ecto_repos: [JllyBot.Repo]
 
+config :jlly_bot, JllyBot.Repo, pool_size: 10
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
